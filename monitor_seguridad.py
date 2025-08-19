1bd89322809bdea8a19113c714212ce09bfc575f
Edgar Javier Díaz Rincón <edgar.diaz@smaingenieros.com.co>
2025-08-18 21:37:03 -0500

    Fix: Corrige error de codificación al leer CSV

--- monitor_seguridad.py
+++ monitor_seguridad.py
@@ -36,7 +36,7 @@ if sheet.row_count == 0 or not sheet.get_all_values():
     sheet.append_row(headers)
 
 # ---------------- LEER MUNICIPIOS ----------------
-df_municipios = pd.read_csv(MUNICIPIOS_FILE)
+df_municipios = pd.read_csv(MUNICIPIOS_FILE, encoding='latin1')
 municipios_list = df_municipios["MUNICIPIO"].tolist()
 
 # ---------------- DATOS EXISTENTES ----------------
